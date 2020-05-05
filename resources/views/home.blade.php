5b9183c6262bf714491d31f8d4178fa99eb9d124
Marolay <marolay.duque@gmail.com>
2020-05-05 16:17:23 -0500

    pagina de datos personales

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,15 @@
 @extends('layout')
 
 @section('content')
+<link rel="stylesheet" href="../public/css/error.css">
+<div class="pagina-info">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Información Personal</div>
+                <div class="card-header p-3 mb-2 bg-success text-white">
+                    <h4> <center><strong>Bienvenido {{ auth()->user()->nombres }}
+                          Aquí tu  Información Personal</strong></center> </div></h4>
 
                 <div class="">
                     @if (session('status'))
@@ -16,30 +20,41 @@
                     
                     <section class="tablas">
                         <article>
-                            <table class="table-bordered table-striped">
+                            <!-- table-bordered table-striped -->
+
+                            <table class=" table">
                                 <tr>
                                     <td><strong>Nombres:</strong></td>
                                     <td>{{ auth()->user()->nombres }}</td>
-                                    <td><strong>Apellidos:</strong></td>
-                                    <td>{{ auth()->user()->apellidos }}</td>
-                                    <td><strong>Documento:</strong></td>
-                                    <td>{{ auth()->user()->documento }}</td>
+                                    
                                 </tr>
                                 <tr>
+                                <td><strong>Documento:</strong></td>
+                                    <td>{{ auth()->user()->documento }}</td>
+                                </tr>
+                                <tr class="" >
+                                    <td><strong>Apellidos:</strong></td>
+                                    <td>{{ auth()->user()->apellidos }}</td>
+                                </tr>
+                                <tr class="" >
                                     <td><strong>Correo:</strong></td>
                                     <td>{{ auth()->user()->email }}</td>
-                                    <td><strong>Teléfono:</strong></td>
-                                    <td>{{ auth()->user()->telefono }}</td>
-                                    <td><strong>Ciudad</strong></td>
-                                    <td>{{ auth()->user()->ciudad }}</td>
+                                   
+                                   
                                 </tr>
+                                <tr class=""  >
+                                <td><strong>Teléfono:</strong></td>
+                                    <td>{{ auth()->user()->telefono }}</td>
+                                </tr>
+                                <tr class=""  > <td><strong>Ciudad</strong></td>
+                                    <td>{{ auth()->user()->ciudad }}</td></tr>
                                 <tr>
                                     <td><strong>Programa:</strong></td>
                                     <td>{{ auth()->user()->programas->pro_nombre }}</td>
                                 </tr>
                             </table>
-                            <a href="" class="btn btn-sm btn-primary mt-3 mb-3 ml-3"><span class="fas fa-user-edit"></span></a>
-                            <a href="{{ action('EstudiantesController@index') }}" class="btn btn-sm btn-primary mt-3 mb-3 ml-3">Ir a proyecto <span class="fas fa-arrow-right"></span></a>
+                            <a href="" class="btn btn-sm btn-success mt-3 mb-3 ml-3"><span class="fas fa-user-edit"></span></a>
+                            <a href="{{ action('EstudiantesController@index') }}" class="btn btn-sm btn-success mt-3 mb-3 ml-3">Ir a proyecto <span class="fas fa-arrow-right"></span></a>
                         </article>
                     </section>
                     
@@ -47,5 +62,6 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
